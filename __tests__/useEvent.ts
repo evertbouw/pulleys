3a88ca92539e3233f3be58ee610540790b2dd760
Evert Bouw <bouw@outlook.com>
2019-05-06 12:42:38 +0200

    add useEvent

---
+++ __tests__/useEvent.ts
@@ -0,0 +1,62 @@
+import { act, renderHook } from "react-hooks-testing-library";
+import { cleanup, fireEvent } from "react-testing-library";
+import { useEvent } from "../src/useEvent";
+
+const leftClick = { button: 0, clientX: 50 };
+
+afterEach(cleanup);
+
+describe("useEvent", () => {
+  it("works", () => {
+    const eventName = "click";
+    const getValue = jest.fn((e: MouseEvent) => e.clientX);
+    const initialState = 0;
+
+    const { result } = renderHook(() =>
+      useEvent({
+        eventName,
+        getValue,
+        initialState
+      })
+    );
+
+    expect(result.current).toBe(initialState);
+
+    act(() => {
+      fireEvent.click(window, leftClick);
+    });
+
+    expect(getValue).toHaveBeenCalledTimes(1);
+
+    expect(result.current).toBe(leftClick.clientX);
+  });
+
+  it("accepts elements", () => {
+    const eventName = "click";
+    const getValue = jest.fn(
+      (e: MouseEvent) => (e.target as HTMLButtonElement).name
+    );
+    const initialState = "foo";
+    const element = document.createElement("button");
+    element.name = "bar";
+
+    const { result } = renderHook(() =>
+      useEvent({
+        eventName,
+        getValue,
+        initialState,
+        element
+      })
+    );
+
+    expect(result.current).toBe(initialState);
+
+    act(() => {
+      element.click();
+    });
+
+    expect(getValue).toHaveBeenCalledTimes(1);
+
+    expect(result.current).toBe(element.name);
+  });
+});
---
+++ src/useEvent.ts
@@ -0,0 +1,65 @@
+import { useCallback, useState } from "react";
+import { useEventListener } from "./useEventListener";
+
+interface UseEvent {
+    <
+        Elem extends MediaQueryList,
+        K extends keyof MediaQueryListEventMap,
+        Value
+    >(args: {
+        eventName: K;
+        getValue: (event: MediaQueryListEventMap[K]) => Value;
+        initialState: Value;
+        element: Elem;
+    }): Value;
+    <
+        Elem extends HTMLElement,
+        K extends keyof HTMLElementEventMap,
+        Value
+    >(args: {
+        eventName: K;
+        getValue: (event: HTMLElementEventMap[K]) => Value;
+        initialState: Value;
+        element: Elem;
+    }): Value;
+    <Elem extends Window, K extends keyof WindowEventMap, Value>(args: {
+        eventName: K;
+        getValue: (event: WindowEventMap[K]) => Value;
+        initialState: Value;
+        element: Elem;
+    }): Value;
+    <K extends keyof WindowEventMap, Value>(args: {
+        eventName: K;
+        getValue: (event: WindowEventMap[K]) => Value;
+        initialState: Value;
+    }): Value;
+}
+
+export const useEvent: UseEvent = <
+    Elem extends Window | undefined,
+    K extends keyof WindowEventMap,
+    Value
+>({
+    eventName,
+    getValue,
+    initialState,
+    element = window,
+}: {
+    eventName: K;
+    getValue: (event: WindowEventMap[K]) => Value;
+    initialState: Value;
+    element?: Elem;
+}): Value => {
+    const [value, setValue] = useState(initialState);
+
+    const listener = useCallback(
+        event => {
+            setValue(getValue(event));
+        },
+        [getValue],
+    );
+
+    useEventListener({ eventName, listener, element });
+
+    return value;
+};
