e011449865d792cefeb757f79e18d03bd38b18ff
Evert Bouw <bouw@outlook.com>
2022-10-03 17:45:55 +0200

    spy in console instead

--- src/utils/createSafeContextHook.test.tsx
+++ src/utils/createSafeContextHook.test.tsx
@@ -1,23 +1,18 @@
 import { renderHook } from '@testing-library/react';
 import { createContext } from 'react';
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { createSafeContextHook } from './createSafeContextHook';
 
-let _console_error: typeof console.error;
-
 describe('createSafeContextHook', () => {
-    beforeEach(() => {
-        _console_error = console.error;
-        console.error = () => {
-            // not empty
-        };
-    });
-
     afterEach(() => {
-        console.error = _console_error;
+        vi.restoreAllMocks();
     });
 
     it('works', () => {
+        vi.spyOn(console, 'error').mockImplementation(() => {
+            // not empty
+        });
+
         const myContext = createContext(undefined);
         const useMyContext = createSafeContextHook(myContext);
 
@@ -27,11 +22,17 @@ describe('createSafeContextHook', () => {
     });
 
     it('works', () => {
-        const myContext = createContext<string|undefined>(undefined);
+        const myContext = createContext<string | undefined>(undefined);
         const useMyContext = createSafeContextHook(myContext);
 
         expect(() => {
-            renderHook(() => useMyContext(), { wrapper: ({ children }) => <myContext.Provider value="foo">{ children }</myContext.Provider> });
+            renderHook(() => useMyContext(), {
+                wrapper: ({ children }) => (
+                    <myContext.Provider value="foo">
+                        {children}
+                    </myContext.Provider>
+                ),
+            });
         }).not.toThrowError('Context was used without a provider');
     });
 });
