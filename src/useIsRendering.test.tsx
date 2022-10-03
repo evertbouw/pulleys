b7f6370edd9e8ed6edfa72c04fe1fb01ebe697bd
Evert Bouw <bouw@outlook.com>
2022-10-03 15:36:02 +0200

    add test for useIsRendering

---
+++ src/useIsRendering.test.tsx
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import { useEffect, useLayoutEffect } from "react";
+import { describe, expect, it } from "vitest";
+import { useIsRendering } from "./useIsRendering";
+
+describe("useIsRendering", () => {
+  it("works", () => {
+    const MockComponent = () => {
+        // eslint-disable-next-line testing-library/render-result-naming-convention
+        const isRenderingRef = useIsRendering();
+
+        expect(isRenderingRef.current).toBe(true);
+
+        useLayoutEffect(() => {
+            expect(isRenderingRef.current).toBe(false);
+        }, []);
+
+        useEffect(() => {
+            expect(isRenderingRef.current).toBe(false);
+        }, []);
+
+        return null;
+    }
+
+    render(<MockComponent />);
+
+    expect.assertions(3);
+  });
+});
