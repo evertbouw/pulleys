12082f172ccaf9d445c4c008e36e0df9e3346381
Evert Bouw <bouw@outlook.com>
2023-02-08 11:21:37 +0100

    add eslint-plugin-vitest

--- src/useForceUpdate.test.tsx
+++ src/useForceUpdate.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { useRef } from 'react';
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { useForceUpdate } from './useForceUpdate';
 
 describe('useForceUpdate', () => {
@@ -25,8 +25,12 @@ describe('useForceUpdate', () => {
         };
         render(<MockComponent />);
 
+        expect(screen.getByText('0')).toBeDefined();
         await user.click(screen.getByText('0'));
+        expect(screen.getByText('1')).toBeDefined();
         await user.click(screen.getByText('1'));
+        expect(screen.getByText('2')).toBeDefined();
         await user.click(screen.getByText('2'));
+        expect(screen.getByText('3')).toBeDefined();
     });
 });
