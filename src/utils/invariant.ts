bc0a0bc61b25d9af280a552294f24b355fa97ad1
Evert Bouw <evert@smanwaarnemingen.nl>
2023-03-31 13:38:37 +0200

    rewrite invariant to arrow

--- src/utils/invariant.ts
+++ src/utils/invariant.ts
@@ -1,6 +1,5 @@
-export function invariant(
-    condition: boolean,
-    message = 'Invariant violation',
-): asserts condition {
+type Invariant = (condition: unknown, message: string) => asserts condition;
+
+export const invariant: Invariant = (condition, message = 'Invariant violation') => {
     if (!condition) throw new Error(message);
 }
